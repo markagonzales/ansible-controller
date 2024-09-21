0d4179fcad061df9ccc46538adca310d833d31fb
markagonzales <mark@spinbit.org>
2024-09-21 14:22:56 -0500

    begins to establish a functional ansible base image

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 
-ansible-builder build --prune-images --build-arg EE_BASE_IMAGE=registry.access.redhat.com/ubi8/ubi:latest --tag ansible:8-2.14
+ansible-builder build --no-cache --prune-images --build-arg EE_BASE_IMAGE=registry.access.redhat.com/ubi8/ubi:latest --tag hooplad/ansible:8-2.14
+ansible-builder build --no-cache --prune-images --build-arg EE_BASE_IMAGE=registry.access.redhat.com/ubi9/ubi:latest --tag hooplad/ansible:9-2.14
 
 ansible-builder build --prune-images --build-arg EE_BASE_IMAGE=registry.access.redhat.com/ubi9/ubi:latest --tag ansible:9-2.14
