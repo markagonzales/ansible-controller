bdc2cb3fd083a06753d58e513f34e5fe815dd298
markagonzales <mark@spinbit.org>
2024-12-01 12:12:06 -0600

    updates build script for all target images

--- build.sh
+++ build.sh
@@ -3,3 +3,11 @@
 ansible-builder build --no-cache --prune-images \
     --build-arg EE_BASE_IMAGE=quay.io/rockylinux/rockylinux:9 \
     --tag hooplad/ansible:rocky9-2.14
+
+ansible-builder build --no-cache --prune-images \
+    --build-arg EE_BASE_IMAGE=quay.io/rockylinux/rockylinux:9 \
+    --tag hooplad/ansible:rocky8-2.14
+
+docker push hooplad/ansible:rocky9-2.14
+
+docker push hooplad/ansible:rocky8-2.14
