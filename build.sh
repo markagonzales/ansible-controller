db28286bdd2b7f900a9be84ca71240db591a8d34
markagonzales <mark@spinbit.org>
2024-12-01 14:14:09 -0600

    forces docker runtime

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-ansible-builder build --no-cache --prune-images \
+ansible-builder build --no-cache --prune-images --container-runtime=docker \
     --build-arg EE_BASE_IMAGE=quay.io/rockylinux/rockylinux:9 \
     --tag hooplad/ansible:rocky9-2.14
 
-ansible-builder build --no-cache --prune-images \
+ansible-builder build --no-cache --prune-images --container-runtime=docker \
     --build-arg EE_BASE_IMAGE=quay.io/rockylinux/rockylinux:9 \
     --tag hooplad/ansible:rocky8-2.14
 
