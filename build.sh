9e6c17cbc9a13d4ec89bc41a7113e280d32f7828
Mark Gonzales <markagonzales@users.noreply.github.com>
2025-01-27 15:05:03 -0600

    Update build.sh
    updates build script to build for R8

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ ansible-builder build --no-cache --prune-images --container-runtime=docker \
     --tag hooplad/ansible:rocky9-2.14
 
 ansible-builder build --no-cache --prune-images --container-runtime=docker \
-    --build-arg EE_BASE_IMAGE=quay.io/rockylinux/rockylinux:9 \
+    --build-arg EE_BASE_IMAGE=quay.io/rockylinux/rockylinux:8 \
     --tag hooplad/ansible:rocky8-2.14
 
 docker push hooplad/ansible:rocky9-2.14
